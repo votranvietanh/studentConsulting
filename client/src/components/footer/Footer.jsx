18f7ee3c20ff09b21359009c8a78a5bc9c8c7ea7
votranvietanh <v0tr4nvietanh@gmail.com>
2024-02-21 23:21:39 +0700

    update

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -8,8 +8,9 @@ const Footer = () => {
                 <div>
                     <p className="font-roboto font-bold mb-2">GVHD: Nguyễn Hữu Trung</p>
                     <p className="font-roboto ">Nhóm 1:</p>
-                    <p className="font-roboto ml-3">Trần Nhật Hào - 20110471</p>
-                    <p className="font-roboto ml-3">Trần Thanh Phương - 20110547</p>
+                    <p className="font-roboto ml-3">Lê Anh Dũng </p>
+                    <p className="font-roboto ml-3">Việt Anh </p>
+                    {/* // Viet ANh */}
                 </div>
             </div>
         </footer>
